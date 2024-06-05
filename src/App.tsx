dadd20e5a641ee48b15d384b9e263bade670733c
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2024-06-05 16:46:28 +0200

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import logo from './assets/logo.png';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,11 +8,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={logo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -32,4 +28,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
