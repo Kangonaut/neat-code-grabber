2619405e43806a0db391b1155774f88ead7e2a71
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2024-06-06 13:36:15 +0200

    add programming language extraction and selection

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,3 +1,42 @@
+const availableProgrammingLanguages = [
+    "C++", "Java", "Python", "Python3", "C", "C#",
+    "JavaScript", "TypeScript", "PHP", "Swift",
+    "Kotlin", "Dart", "Go", "Ruby", "Scala", "Rust",
+    "Racket", "Erlang", "Elixir", "Pandas", "MySQL",
+    "MS SQL Server", "Oracle", "PostgreSQL"
+];
+
+function getProblem() {
+    // get all anchor tags
+    const anchors = document.querySelectorAll("a");
+
+    // find problem title anchor tag
+    for (const anchor of anchors) {
+        const re = /(\d+).\s+([\w\s]+)/;
+        const result = anchor.textContent.match(re);
+
+        if (result) {
+            return {
+                id: Number.parseInt(result[1]),
+                title: result[2],
+            };
+        }
+    }
+}
+
+function getProgrammingLanguage() {
+    const buttons = document.querySelectorAll("button");
+
+    for (const button of buttons) {
+        const lang = button.textContent;
+
+        // check if valid programming language
+        if (availableProgrammingLanguages.includes(lang)) {
+            return lang;
+        }
+    }
+}
+
 function getEditorContent() {
     function getEditorDiv() {
         const divs = document.querySelectorAll("div");
@@ -27,7 +66,12 @@ function getEditorContent() {
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "getEditorContent") {
         const content = getEditorContent();
-        console.log(`editor content: ${content}`);
         sendResponse({ content });
+    } else if (request.action === "getProblem") {
+        const problem = getProblem();
+        sendResponse({ problem });
+    } else if (request.action === "getProgrammingLanguage") {
+        const language = getProgrammingLanguage();
+        sendResponse({ language });
     }
 });
